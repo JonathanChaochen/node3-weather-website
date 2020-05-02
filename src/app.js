78a54c968189e955692c6a7d83ade86185f74d46
Chao chen <chaochen42@gmail.com>
2020-05-02 20:13:01 +1200

    Add about text

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.get('/help', (req, res) => {
   res.render('help', {
     helpText: 'This is some helpful text',
     title: 'Help',
-    name: 'Joe Black',
+    name: 'Chao Chen',
   });
 });
 
